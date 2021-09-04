019e7206386aa8cba986aaa1798c3feb64981f8c
tripathi-nik-kiwi <nikhil.tripathi@kiwitech.com>
2021-09-05 00:35:19 +0530

    Worked on improving redirections

--- src/custom/WebConfig.js
+++ src/custom/WebConfig.js
@@ -1,11 +1,17 @@
-import {useMemo} from 'react';
+import {useMemo,useState} from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 const WebConfig = () =>{
-  let userID = localStorage.getItem('usID');
+  const [user,setUser] = useState(null);
   let log = useSelector(state => state.agentReducer);
-  const fetchRecord = useMemo(()=>{
-    alert("hello the change in state");
+  useMemo(()=>{
+    let userID = localStorage.getItem('usID');
+    setUser(!log._id&&log.validate?log.validate:null);
+    //console.log(log)
   },[log]);
-  return `user id of the site ${userID}`;
+  if(user===true){
+    console.log("now hits the dispatch");
+    setUser(null);
+  }
+  //return `user id of the site ${userID}`;
 }
 export default WebConfig;
