beacb8529f8b1346da90346b649b8b39020901af
tripathi-nik-kiwi <nikhil.tripathi@kiwitech.com>
2021-10-19 22:10:03 +0530

    Adding product page

--- src/config/webPath.js
+++ src/config/webPath.js
@@ -3,7 +3,8 @@ const PATHS = {
   REGISTER: '/admin/register',
   LOGIN: '/admin/login',
   ACCOUNT: '/admin/account',
-  PROFILE: '/admin/profile'
+  PROFILE: '/admin/profile',
+  PRODUCT: '/admin/product'
 };
 
 export default PATHS;
